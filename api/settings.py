f5a5a85e2706de61e37fc48230ab33d08684a68a
unknown <srijandas18.sd@gmail.com>
2022-07-19 01:58:03 +0530

    deploy

--- api/settings.py
+++ api/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv("HOST_NAME"), "127.0.0.1", "localhost:8000"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", "localhost:8000"]
 
 
 # Application definition
